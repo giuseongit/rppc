1426c9708e605f405f2321c6360d52d6b0ba751a
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2015-05-26 14:03:37 +0200

    receiver fix

--- lib/rppc/net/receiver.rb
+++ lib/rppc/net/receiver.rb
@@ -6,13 +6,13 @@ require "ipaddr"
 # @author Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
 class Receiver
     include Observable
-    # Multicast address to reach all connected devices inside the network
-    MULTICAST_ADDR = "224.0.0.1"
-    # Bind address
-    BIND_ADDR = "0.0.0.0"
-    
+    MULTICAST_ADDR = "224.0.0.1" # Multicast address to reach all connected devices inside the network
+    BIND_ADDR = "0.0.0.0" # Bind address
+
     # Class constructor
-    # @param port [Fixnum] the port on which listen
+    #
+    # @param udp [Fixnum] the port on which listen udp connections
+    # @param tcp [Fixnum] the port on which listen tcp connections
     def initialize(udp, tcp)
         @udp_port = udp
         @tcp_port = tcp
@@ -21,17 +21,20 @@ class Receiver
     end
     
     # Tells if the receiver is running
+    #
     # @return [Boolean] the running state
     def is_running?
         @running_udp != nil && @running_tcp != nil
     end
     
     # Register observer that handles received messages
-    # obj MUST have the method receive(data, addrinfo)
+    #
+    # @param obj MUST have the method receive(data, addrinfo)
     def register(obj)
         add_observer(obj,:receive)
     end
-
+    
+    # Starts the server
     def start_listen
         if @running_udp != nil || @running_tcp != nil
             raise "Server already running!"
@@ -41,6 +44,7 @@ class Receiver
         start_listen_udp
     end
     
+    # Stops the server
     def stop_listen
         if @running_udp == nil || @running_tcp == nil
             raise "Server not running!"
@@ -51,7 +55,7 @@ class Receiver
     end
 
 private
-
+    # Starts the udp component
     def start_listen_udp
         @udp_server = socket = UDPSocket.new
         membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
@@ -72,13 +76,14 @@ private
         end
     end
     
-    # Stops the server
+    # Stops the udp component
     def stop_listen_udp
         Thread.kill(@running_udp)
         @running_udp = nil
         @udp_server.close
     end
-
+    
+    # Starts the tcp component
     def start_listen_tcp
         @tcp_server = TCPServer.new @tcp_port
         @running_tcp = Thread.new do
@@ -91,13 +96,18 @@ private
             end
         end
     end
-
+    
+    # Stops the udp component
     def stop_listen_tcp
         Thread.kill(@running_tcp)
         @running_tcp = nil
         @tcp_server.close
     end
-
+    
+    # Notifies the registered observers
+    #
+    # @param data [String] the received string
+    # @param addrinfo [Addrinfo] the client's info
     def receive_packet(data, addrinfo)
         changed
         notify_observers(data, addrinfo)
--- spec/net/comunication_test_spec.rb
+++ spec/net/comunication_test_spec.rb
@@ -20,37 +20,58 @@ class MockReceiver
     end
 end
 
-describe "Comunication" do
+describe "Communication" do
     before :all do
-        port = 5000
+        udp = 5000
+        tcp = 5001
         @mock = MockReceiver.new
-        @sender = Sender.new(port)
-        @receiver = Receiver.new(port)
-        @msg = "test"
+        @sender = Sender.new(udp, tcp)
+        @receiver = Receiver.new(udp, tcp)
+        @udp_msg = "test"
+        @tcp_msg = "__test__"
+        @count = 0
+        @receiver.register(@mock)
     end
 
-    it "entities can comunicate" do
-        count = @mock.count_received_element
+    it "entities can communicate via udp" do
+        @count = @mock.count_received_element
 
-        @receiver.register(@mock)
-
-        expect(@mock.count_received_element).to eq(count)
+        expect(@mock.count_received_element).to eq @count
         @receiver.start_listen
 
-        expect(@mock.count_received_element).to eq(count)
+        expect(@mock.count_received_element).to eq @count
 
-        @sender.send(@msg, "127.0.0.1")
+        @sender.send_udp(@udp_msg, "127.0.0.1")
 
         sleep 0.5
         
-        expect(@mock.count_received_element).to eq(count + 1)
+        expect(@mock.count_received_element).to eq(@count + 1)
+
+        @receiver.stop_listen
     end
     
-    it "recieves right test" do
-        expect(@mock.get_last).to eq(@msg)
+    it "recieves right message via udp" do
+        expect(@mock.get_last).to eq @udp_msg
     end
 
-    after :all do
+    it "entities can communicate via tcp" do
+        @count = @mock.count_received_element
+
+        expect(@mock.count_received_element).to eq @count
+        @receiver.start_listen
+
+        expect(@mock.count_received_element).to eq @count
+
+        @sender.send_tcp(@tcp_msg, "127.0.0.1")
+
+        sleep 0.5
+        
+        expect(@mock.count_received_element).to eq(@count + 1)
+
         @receiver.stop_listen
+    end
+
+    it "receives right message via tcp" do
+        expect(@mock.get_last).to eq @tcp_msg
     end
 end
