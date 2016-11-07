9f043107083797567a6a0376e84b1f40ef96b16a
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2016-11-07 15:05:34 +0100

    fixed tests

--- spec/core/engine_spec.rb
+++ spec/core/engine_spec.rb
@@ -7,7 +7,7 @@ class MockGui
         @elems = []
     end
 
-    def receive(data, node)
+    def received(node, data)
         @elems.push data
     end
 
@@ -20,6 +20,9 @@ class MockGui
     end
 end
 
+class WrongGui
+end
+
 describe Rppc::Core::Engine do
     before do
         @mock = MockGui.new
@@ -28,6 +31,10 @@ describe Rppc::Core::Engine do
 
     it "can instantiate" do
         @engine != nil
+    end
+
+    it "raises error on a gui which has not the received method" do
+        expect{Rppc::Core::Engine.new(WrongGui.new)}.to raise_error Rppc::Errors::WrongObjectError
     end
 
     it "has a receiver instance" do
@@ -44,6 +51,10 @@ describe Rppc::Core::Engine do
         expect{ @engine.discover() }.not_to raise_error
     end
 
+    it "can send announce exit packet" do
+        expect{ @engine.announce_exit() }.not_to raise_error
+    end
+
     it "responds to node functions" do
         expect(@engine.respond_to?(:new_node)).to eq true
         expect(@engine.respond_to?(:get_nodes)).to eq true
@@ -51,15 +62,20 @@ describe Rppc::Core::Engine do
     end
 
     it "receives first message and adds node" do
-        msg = Rppc::Net::Packet::PacketData::HELO
+        helo = Rppc::Net::Packet::PacketData::HELO
+        msg = "#{helo}|#|"
         addrinfo = ["", "", "127.0.0.1"]
-        expect(@engine.instance_variable_get(:@known_nodes).length).to eq 0
+
+        known = @engine.instance_variable_get(:@known_nodes)
+
+        expect(known.length).to eq 0
         @engine.receive(msg, addrinfo)
-        expect(@engine.instance_variable_get(:@known_nodes).length).to eq 1
+        expect(known.length).to eq 1
     end
 
     it "raises an exception on double node add" do
-        msg = Rppc::Net::Packet::PacketData::HELO
+        helo = Rppc::Net::Packet::PacketData::HELO
+        msg = "#{helo}|#|"
         addrinfo = ["", "", "127.0.0.1"]
         @engine.receive(msg, addrinfo)
 
@@ -67,20 +83,67 @@ describe Rppc::Core::Engine do
     end
 
     it "raises error on crafted hello message"  do
-        msg = "nope"
+        msg = "nope|#|"
         addrinfo = ["", "", "127.0.0.1"]
         expect{@engine.receive(msg, addrinfo)}.to raise_error Rppc::Errors::CraftedMessageError
+
+        helo = Rppc::Net::Packet::PacketData::HELO
+        msg = "#{helo}|#|crafted"
+        expect{@engine.receive(msg, addrinfo)}.to raise_error Rppc::Errors::CraftedMessageError
+    end
+
+    it "sets correct username" do
+        helo = Rppc::Net::Packet::PacketData::HELO
+        usr_name = Rppc::Net::Packet::PacketData::USR_NAME
+
+        username = "testUserName"
+
+        msg = "#{helo}|#|"
+        addrinfo = ["", "", "127.0.0.1"]
+        @engine.receive(msg, addrinfo)
+
+        known = @engine.instance_variable_get(:@known_nodes)
+
+        expect(known.last.username).to be_nil
+        msg = "#{usr_name}|#|#{username}"
+        @engine.receive(msg, addrinfo)
+        expect(known.last.username).not_to be_nil
+        expect(known.last.username).to eq username
+    end
+
+    it "sets correct state" do
+        helo = Rppc::Net::Packet::PacketData::HELO
+        usr_state = Rppc::Net::Packet::PacketData::USR_STATE
+
+        username = "testUserState"
+
+        msg = "#{helo}|#|"
+        addrinfo = ["", "", "127.0.0.1"]
+        @engine.receive(msg, addrinfo)
+
+        known = @engine.instance_variable_get(:@known_nodes)
+
+        expect(known.last.state).to be_nil
+        msg = "#{usr_state}|#|#{username}"
+        @engine.receive(msg, addrinfo)
+        expect(known.last.state).not_to be_nil
+        expect(known.last.state).to eq username
     end
 
     it "removes node correctly" do
         ip = "127.0.0.1"
-        msg = Rppc::Net::Packet::PacketData::HELO
+        helo = Rppc::Net::Packet::PacketData::HELO
+        bye = Rppc::Net::Packet::PacketData::BYE
+        msg = "#{helo}|#|"
         addrinfo = ["", "", ip]
         @engine.receive(msg, addrinfo)
+        msg = "#{bye}|#|"
 
-        expect(@engine.instance_variable_get(:@known_nodes).length).to eq 1
-        expect{@engine.remove_node(ip)}.to_not raise_error
-        expect(@engine.instance_variable_get(:@known_nodes).length).to eq 0
+        known = @engine.instance_variable_get(:@known_nodes)
+
+        expect(known.length).to eq 1
+        expect{@engine.receive(msg, addrinfo)}.to_not raise_error
+        expect(known.length).to eq 0
     end
 
     it "raises error when trying to remove a non-existing node" do
